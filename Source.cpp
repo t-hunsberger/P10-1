d6fac3234e453e520891b23ea87fbaf3541d3a40
t-hunsberger <t-hunsberger@onu.edu>
2024-04-11 09:42:20 -0400

    hopefully finished scramble function

--- Source.cpp
+++ Source.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
+#include <string>
+#include <cstdlib>
+#include <ctime>
 
 using namespace std;
+
+string scramble(string word) {
+	if (word.length() <= 3) {
+		return word;
+	}
+
+	int index1 = rand() % (word.length() - 2) + 1;
+	int index2;
+	do {
+		index2 = rand() % (word.length() - 2) + 1;
+	} while (index2 == index1);
+
+	char temp = word[index1];
+	word[index1] = word[index2];
+	word[index2] = temp;
+
+	return word;
+}
 
 int main() {
 
